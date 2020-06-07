876c0226fe63d38429f711152126d312a9aab932
akademik <pvv2606@gmail.com>
2020-06-07 09:55:38 +0300

    Initial Commit

---
+++ AuthPage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AuthPage
+//
+//  Created by Akademik on 07.06.2020.
+//  Copyright © 2020 Viktor Pryima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
